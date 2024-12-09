b6d015a98ed8932673c93e86dd059b5ce70939e5
Hakha013 <leonardosaturnino013@gmail.com>
2024-12-09 14:33:46 -0300

    A Curriculum Vitae Project

---
+++ assets/js/acordeon.js
@@ -0,0 +1,17 @@
+const acordTrigger = document.querySelectorAll('.acordeon .trigger');
+
+Array.prototype.forEach.call(acordTrigger, (trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const acordeon = trigger.parentElement;
+
+        const isOpen = acordeon.classList.contains('open');
+        
+        if (isOpen) {
+            acordeon.classList.remove('open');
+        } else {
+            acordeon.classList.add('open');
+        }
+    });
+});
+
+
