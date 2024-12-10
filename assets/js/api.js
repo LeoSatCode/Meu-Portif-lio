15df58727be3372115c45f3fc8db19126dcd7bc2
Hakha013 <leonardosaturnino013@gmail.com>
2024-12-10 13:36:36 -0300

    adjust profile.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileDate() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/refs/heads/projeto-base/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/LeoSatCode/Meu-Portif-lio/refs/heads/main/data/profile.json?token=GHSAT0AAAAAAC2VKTZNBNDYVFDSI6Z77YFCZ2YNTVA'
     const fetching = await fetch(url)
     return await fetching.json()
 }
