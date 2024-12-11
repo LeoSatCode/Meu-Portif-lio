0f2331798fddb242068f26cb629046fb4820d0e8
Hakha013 <leonardosaturnino013@gmail.com>
2024-12-11 08:39:53 -0300

    hard skills improvments

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,10 +28,17 @@ function updateSoftSkills(profileData) {
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile-skills-hardskills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}"></li>`).join('')
+}
+
+
 
 (async () => {
         const profileData = await fetchProfileData()
         updateProfile(profileData)
         updateSoftSkills(profileData)
+        updateHardSkills(profileData)
   
 })()
