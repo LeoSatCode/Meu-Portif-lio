1e9f68362676e5bf856d3fdf61c427e90416844f
Hakha013 <leonardosaturnino013@gmail.com>
2024-12-11 10:38:44 -0300

    some changes whit api integration and css improvments

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,11 +16,11 @@ function updateProfile(profileData) {
     phone.innerText = profileData.phone
     phone.href = `tel:${profileData.phone}`
 
-    const email= document.getElementById('profile.email')
+    const email = document.getElementById('profile.email')
     email.innerText = profileData.email
     email.href = `mailto:${profileData.phone}`
 
-    
+
 }
 
 function updateSoftSkills(profileData) {
@@ -33,12 +33,50 @@ function updateHardSkills(profileData) {
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}"></li>`).join('')
 }
 
+function updateLenguages(profileData) {
+    const lenguages = document.getElementById('profile.lenguages')
+    lenguages.innerHTML = profileData.lenguages.map(leng => `<li>${leng}</li>`).join('')
+}
 
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(port => {
+        return `
+        <li>
+          <h3 ${port.git ? 'class="github"' : ''}>${port.name}</h3>
+          <a href="${port.url}" target="_blank">${port.url}</a>
+        </li>
+        `
+    }).join('')
+}
+
+function updateExperience(profileData) {
+    const experience = document.getElementById('profile.exp')
+
+    experience.innerHTML = profileData.experience.map(exp => {
+        return `
+        
+          <li>
+              <h3 class="title">${exp.name}</h3>
+                  <p class="period">${exp.period}</p>
+                  <p>
+                     ${exp.description}
+                    </p>
+                </li>
+                
+                        `
+    }).join('')
+
+
+}
 
 (async () => {
-        const profileData = await fetchProfileData()
-        updateProfile(profileData)
-        updateSoftSkills(profileData)
-        updateHardSkills(profileData)
-  
+    const profileData = await fetchProfileData()
+    updateProfile(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    updateLenguages(profileData)
+    updatePortfolio(profileData)
+    updateExperience(profileData)
+
 })()
